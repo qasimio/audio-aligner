5d1a39f995ff508a675ec2132aa7fef0eb187120
qasimio <amkassim444@gmail.com>
2026-06-04 23:38:09 +0500

    Speech detector - detch timestamps for speech -- silence -- speech

--- src/speech_detector.py
+++ src/speech_detector.py
@@ -1,4 +1,25 @@
+import torch 
+import soundfile as sf
+
 from silero_vad import (
     load_silero_vad,
-    get_speech_timestamps
+    get_speech_timestamps,
 )
+
+model = load_silero_vad()
+
+def detect_speech(audio_path):
+    audio, sr = sf.read(audio_path)
+
+    if len(audio.shape) > 1:
+        audio = audio.mean(axis=1)
+
+    audio = torch.tensor(audio)
+
+    timestamps = get_speech_timestamps(
+        audio,
+        model,
+        sampling_rate=sr,
+    )
+
+    return timestamps, sr
